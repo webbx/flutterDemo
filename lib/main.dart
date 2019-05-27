cb17d787f5c5567b6e88be72d2c37f69bb6d1828
wangbo <bo.wang1016@outlook.com>
2019-05-27 21:39:34 +0800

    message:Text组件

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
-        return new MaterialApp(
-            title: 'Welcome to Flutter',
-            home: new Scaffold(
-                appBar: new AppBar(
-                    title: new Text('Welcome to Flutter'),
-                ),
-                body: new Center(
-                    child: new Text('Hello World'),
-                ),
-            ),
+        return MaterialApp (
+            title: 'Text Demo',
+            home: Scaffold(
+                body: Center(
+                    child: Text(
+                        'hello wang, like flutterhello wang, like flutterhello wang, like flutter',
+                        textAlign: TextAlign.left,
+                        // 显示最大行数
+                        maxLines: 1,
+                        // 超过一刚=行显示...
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Color.fromARGB(255, 244, 100, 125),
+                            decoration: TextDecoration.underline,
+                            decorationStyle: TextDecorationStyle.dashed
+                        ),
+                    ),
+                )
+            )
         );
     }
 }
