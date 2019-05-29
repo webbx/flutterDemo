f74adf5c46df3d1dcb31075414f97be2738b99b5
wangbo <bo.wang1016@outlook.com>
2019-05-29 23:40:04 +0800

    message:导航组件

---
+++ lib/nav.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+    runApp(MaterialApp(
+        title: '导航首页',
+        home: new MyApp(),
+    ));
+}
+
+class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp (
+            title: '导航首页 Demo',
+            home: Scaffold(
+                appBar: new AppBar(title: new Text('导航首页')),
+                body: Center(
+                    child: RaisedButton(
+                        child: new Text('跳转'),
+                        onPressed: (){
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => new ListViewDynamic()));
+                        }
+                    ),
+                )
+            )
+        );
+    }
+}
+
+class ListViewDynamic extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return new Scaffold(
+            appBar: new AppBar(title: new Text('导航详情')),
+            body: Center(
+                child: RaisedButton(
+                    child: new Text('返回'),
+                    onPressed: (){
+                        Navigator.pop(context);
+                    }
+                    
+                ),
+            ),
+        );
+    }
+}
